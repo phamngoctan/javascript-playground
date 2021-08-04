a1d6fb23674ea3028e164bfdba60fb28e9ac1175
Tan Pham <pnt.dtqn1991@gmail.com>
2021-08-04 14:38:19 +0700

    Add the todos app using VueJS

--- React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/4-simple-todo-app-using-vuejs/index.js
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/4-simple-todo-app-using-vuejs/index.js
@@ -1,50 +1,38 @@
-let inputEl = $('input');
-let buttonEl = $('button');
-let ulEl = $('ul');
-
-let todos = [];
-buttonEl.click(addTodo);
-
-function addTodo() {
-    let userInput = inputEl.val();
-    if (userInput.trim() == '') {
-        return;
-    }
-    console.log('User input value: ' + userInput);
-    let newTodo = initNewTopo(userInput)
-    todos.push(newTodo);
-    
-    // let todoLi = createLiTodo(userInput);
-    $('<li>' + userInput + '</li>')
-        .appendTo(ulEl)
-        .attr('dataset-id', newTodo.id)
-        .click(removeTodo);
-
-    pointBackToInput();
-}
-
-function initNewTopo(userInput) {
-    return {
-        id: _.uniqueId(),
-        value: userInput
-    };
-}
-
-function pointBackToInput() {
-    inputEl.focus();
-    inputEl.val('');
-}
-
-function removeTodo(event) {
-    let clickedLi = $(this);
-    let clickedLiId = clickedLi.attr('dataset-id');
-    console.log(clickedLiId);
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i]['id']== clickedLiId) {
-            todos.splice(i, 1);
-            break;
+new Vue({
+    el: '#app',
+    data: {
+        todos: [],
+        userInput: ''
+    },
+    methods: {
+        addTodo: function() {
+            // this in VueJS, makes sure it point to the Vue instance
+            if (this.userInput.trim() == '') {
+                return;
+            }
+            console.log('User input value: ' + this.userInput);
+            let newTodo = initNewTopo(this.userInput);
+            function initNewTopo(userInput) {
+                return {
+                    id: _.uniqueId(),
+                    value: userInput
+                };
+            }
+            this.todos.push(newTodo);
+            pointBackToInput.apply(this);
+            function pointBackToInput() {
+                this.$refs.inputText.focus();
+                this.userInput = "";
+            }
+        },
+        removeTodo(todoItem) {
+            for (let i = 0; i < this.todos.length; i++) {
+                if (this.todos[i]['id']== todoItem.id) {
+                    this.todos.splice(i, 1);
+                    break;
+                }
+            }
+            console.log(this.todos);
         }
     }
-    console.log(todos);
-    clickedLi.remove();
-}
+})
