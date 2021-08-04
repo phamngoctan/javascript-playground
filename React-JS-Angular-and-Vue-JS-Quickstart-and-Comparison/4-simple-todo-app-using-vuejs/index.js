90c7e0b3901184e3283b7922e8359f54a96606f3
Tan Pham <pnt.dtqn1991@gmail.com>
2021-08-04 13:49:42 +0700

    Refactor code

---
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/4-simple-todo-app-using-vuejs/index.js
@@ -0,0 +1,50 @@
+let inputEl = $('input');
+let buttonEl = $('button');
+let ulEl = $('ul');
+
+let todos = [];
+buttonEl.click(addTodo);
+
+function addTodo() {
+    let userInput = inputEl.val();
+    if (userInput.trim() == '') {
+        return;
+    }
+    console.log('User input value: ' + userInput);
+    let newTodo = initNewTopo(userInput)
+    todos.push(newTodo);
+    
+    // let todoLi = createLiTodo(userInput);
+    $('<li>' + userInput + '</li>')
+        .appendTo(ulEl)
+        .attr('dataset-id', newTodo.id)
+        .click(removeTodo);
+
+    pointBackToInput();
+}
+
+function initNewTopo(userInput) {
+    return {
+        id: _.uniqueId(),
+        value: userInput
+    };
+}
+
+function pointBackToInput() {
+    inputEl.focus();
+    inputEl.val('');
+}
+
+function removeTodo(event) {
+    let clickedLi = $(this);
+    let clickedLiId = clickedLi.attr('dataset-id');
+    console.log(clickedLiId);
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i]['id']== clickedLiId) {
+            todos.splice(i, 1);
+            break;
+        }
+    }
+    console.log(todos);
+    clickedLi.remove();
+}
--- React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/README.md
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/README.md
@@ -1,5 +1,10 @@
 
 # Getting started
 
+- The term vanilla script is used to refer to the pure JavaScript :)
+- jQuery
+- Low dash (lo_)
+- VueJS
+
 # Notes
 Learn some more course about Javascript, DOM access, events, ...
