c5a289a28fa07adf1d046c489cc6d5cf5847abfe
Tan Pham <pnt.dtqn1991@gmail.com>
2021-08-05 13:46:27 +0700

    Add the router usage in vue

---
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/vue-framework-intro2/README.md
@@ -0,0 +1,47 @@
+# vue-framework-intro2
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Run your end-to-end tests
+```
+npm run test:e2e
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### install new dependencies
+```
+npm install --save vue-router
+```
+
+#### Trouble shooting
+The above dependency is used for vue v2.0, to work with version > 3.0, use the following command:
+
+```
+npm uninstall vue-router --save
+npm i vue-router@next --save
+```
---
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/vue-framework-intro2/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <router-view></router-view> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/user">User</router-link>
+    </div>
+    <router-view />
+    <hr>
+    <app-info></app-info>
+
+  </div>
+</template>
+
+<script>
+import Info from './components/Info.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import User from './components/User.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    'app-info': Info
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
