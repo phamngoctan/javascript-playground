351ca677166dcce102fc8a622271d6af7056a785
Tan Pham <pnt.dtqn1991@gmail.com>
2021-08-13 09:40:02 +0700

    Add the binding sample code from different components

--- React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/angular-framework-intro/src/app/app.component.ts
+++ React-JS-Angular-and-Vue-JS-Quickstart-and-Comparison/angular-framework-intro/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-framework-intro';
+  name = 'Max';
+  elements:number[] = [];
+
+  onChangeName() {
+    this.name = 'AnnA';
+  }
+
+  onAddElement() {
+    // console.log(typeof this.title);
+    this.elements.push(this.elements.length + 1);
+  }
+
+  getColor(element:number) {
+    return element % 2 === 0 ? 'green' : 'orange';
+  }
+
+  onUserWasClicked(userName: string) {
+    alert(userName);
+  }
 }
